e592c6f04abbe262d4a1864d6fab100eccc4627f
John Anthony Rivera <askinganthony@gmail.com>
2016-06-07 17:27:10 -0700

    Batches are now handled by 'managers' which remove themself from being able to have pending items added after it's already made it's http request

--- ng-resource-manager.js
+++ ng-resource-manager.js
@@ -11,7 +11,7 @@ angular.module('ng-resource-manager', [])
             return {
                 exists: function(id) {
                     if (service.exists(resourceType, id)){
-                        // console.log(resourceType+' '+id+' already exists.');
+                        console.log(resourceType+' '+id+' already exists.');
                         return true;
                     }
                     
@@ -19,20 +19,21 @@ angular.module('ng-resource-manager', [])
                 },
                 fetch: function(id) {
                     if(service.exists(resourceType, id)) {
-                        // console.log(resourceType+' '+id+' - fetched.');
+                         console.log(resourceType+' '+id+' - fetched.');
                         return cache[resourceType][id];
                     }
-                    // console.log(resourceType+' '+id+' - not fetched.');
+                    console.log(resourceType+' '+id+' - not fetched.');
                     return false;
                 },
                 store: function(id, resource) {
-                    // console.log(resourceType+' '+resource+' - stored.');
+                    console.log(resourceType+' '+resource+' - stored.');
                     resourceTypeCache[id] = resource;
                 }, 
                 merge: function(resourceBatch) {
-                    // console.log('merge completed');
+                    console.log('merge completed');
                     angular.merge(resourceTypeCache, resourceBatch);
-                }
+                },
+                
             };
         };
         
@@ -62,47 +63,67 @@ angular.module('ng-resource-manager', [])
     function ($q, $timeout){
         var service = this;
         
+        
+        
         service.create = function(collectorParams) {
             var httpBatchRequest = collectorParams.httpBatchRequest;
             var cache = collectorParams.cache;
             
-            var pendingRequests = {};
-            var requestPending = false;
+            var availableManager;
             
-            var assimilate = function (newBatch) { // https://upload.wikimedia.org/wikipedia/en/a/a1/Picard_as_Locutus.jpg
-                cache.merge(newBatch);
-                for (var id in newBatch) {
-                    var promises = pendingRequests[id];
-                    promises.forEach(function (promise) {
-                        promise.resolve( cache.fetch(id) );
+            var createManager = function(){
+                var pendingRequests = {};
+                
+                var assimilate = function (newBatch) { // https://upload.wikimedia.org/wikipedia/en/a/a1/Picard_as_Locutus.jpg
+                    cache.merge(newBatch);
+                    console.log(cache);
+                    for (var id in newBatch) {
+                        var promises = pendingRequests[id];
+                            if (promises) {
+                                promises.forEach(function (promise) {
+                                    promise.resolve( cache.fetch(id) );
+                                });
+                            } else { console.log('weird, could not fetch '+promises +' which had an id of '+id) }
+                            delete pendingRequests[id];
+                    }
+                    
+                };
+
+                var fetch = function () {
+                    availableManager = false; // Makes sure that nothing is added to this batch after this
+                    var idBatch = Object.keys(pendingRequests);
+                    httpBatchRequest(idBatch).then(function(res){
+                        assimilate(res.data, pendingRequests);
                     });
-                    delete pendingRequests[id];
-                }
-            };
+                };
 
-            var fetch = function () {
-                var idBatch = Object.keys(pendingRequests);
-                httpBatchRequest(idBatch).then(function(res){
-                    assimilate(res.data, pendingRequests);
-                });
-            };
-
-            var pendRequest = function () {
-                requestPending = true;
-                $timeout(function(){
-                    fetch();
-                    requestPending = false;
-                }, 50);
-            };
+                return {
+                    start: function () {
+                        availableManager = this;
+                        $timeout(function(){
+                            fetch();
+                        }, 50);
+                    }, 
+                    add: function(id, promise) {
+                        if (pendingRequests[id]){
+                            pendingRequests[id].push(promise);
+                        } else {
+                            pendingRequests[id] = [promise];
+                        }
+                    }
+                }; 
+            }  
+            
+            
 
             var addToQueue = function (id, promise) {
-                if (pendingRequests[id]){
-                    pendingRequests[id].push(promise);
+                if (availableManager) { 
+                    availableManager.add(id, promise);
                 } else {
-                    pendingRequests[id] = [promise];
+                    var manager = createManager();
+                    manager.start();
+                    manager.add(id,promise);
                 }
-                
-                if (!requestPending) { pendRequest(); }
             };
             
             
@@ -111,7 +132,7 @@ angular.module('ng-resource-manager', [])
                     var q = $q.defer();
 
                     if (cache.exists(id)) { q.resolve(cache.fetch(id)) }
-                    else { addToQueue(id, q) }
+                    else { addToQueue(id, q); console.log('adding to queue: '+id) }
 
                     return q.promise;
                 }
